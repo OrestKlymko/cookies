da8ca1c936da6773fdce653dbe41eb0771948266
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-07-16 17:56:48 +0200

    first commit

---
+++ src/main/java/com/example/demo2/TimeServlet.java
@@ -0,0 +1,36 @@
+package com.example.demo2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+@WebServlet(value = "/time")
+public class HelloResource extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html; charset=utf-8");
+
+		String queryTimeZone = req.getParameter("timezone");
+		if(queryTimeZone==null) queryTimeZone="UTC";
+
+		Date currentDate = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
+		dateFormat.setTimeZone(TimeZone.getTimeZone(queryTimeZone));
+		String currentTime = dateFormat.format(currentDate);
+
+
+
+
+
+		resp.getWriter().write(currentTime);
+		resp.getWriter().close();
+	}
+
+}
