1a7409c09e8edc1d8ee098178f08fcbfe3a65084
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-07-17 19:18:22 +0200

    Update TimezoneValidateFilter.java

--- src/main/java/com/example/demo2/TimezoneValidateFilter.java
+++ src/main/java/com/example/demo2/TimezoneValidateFilter.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter("/time")
 public class TimezoneValidateFilter implements Filter {
+	final String DEFAULT_GMT_TIMEZONE = "UTC";
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -23,27 +24,27 @@ public class TimezoneValidateFilter implements Filter {
 		String timezoneParam = httpRequest.getParameter("timezone");
 
 		if (timezoneParam == null || timezoneParam.isEmpty()) {
-			timezoneParam="UTC";
+			timezoneParam=DEFAULT_GMT_TIMEZONE;
 		}
 
 		if (!isValidTimezone(timezoneParam)) {
 			httpResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid timezone");
 			return;
 		}
-
 		chain.doFilter(request, response);
 	}
 
 	private boolean isValidTimezone(String timezone) {
-		String[] availableTimezones = TimeZone.getAvailableIDs();
-		for (String tz : availableTimezones) {
-			if (tz.equals(timezone)) {
+
+		if (timezone.equals(DEFAULT_GMT_TIMEZONE)) {
+			return true;
+		} else {
+			String id = TimeZone.getTimeZone(timezone).getID();
+			if (!id.equals(DEFAULT_GMT_TIMEZONE)) {
 				return true;
 			}
 		}
 		return false;
 	}
-
-
 
 }
